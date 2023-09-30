8fcb06a5cdcb87e1834ffe32498e0bde8274f4de
shdomi8599 <shdomi@naver.com>
2023-10-01 04:20:48 +0900

    api auth 컨트롤러 구현

--- api/src/api/api.controller.ts
+++ api/src/api/api.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Controller, Get, Post, Request, Res, UseGuards } from "@nestjs/common";
+import { AuthGuard } from "@nestjs/passport";
 import { AuthService } from "src/auth/auth.service";
 import { FavoriteService } from "src/favorite/favorite.service";
 import { PresetService } from "src/preset/preset.service";
@@ -13,9 +14,46 @@ export class ApiController {
     private readonly favoriteService: FavoriteService,
   ) {}
 
-  @Get("user/all")
-  async getAllUser(@Res() res: any) {
-    const users = await this.userService.findAll();
-    return res.json(users);
+  @UseGuards(AuthGuard("local"))
+  @Post("auth/login")
+  async login(@Request() req, @Res({ passthrough: true }) res) {
+    const tokens = await this.authService.login(req.user);
+    const { accessToken, refreshToken } = tokens;
+
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production" ? true : false,
+    });
+
+    return { accessToken };
+  }
+
+  @UseGuards(AuthGuard("jwt"))
+  @Get("auth/refreshToken")
+  async getRefreshToken(@Request() req) {
+    const { address } = req.user;
+    const ret = await this.authService.getRefreshToken(address);
+    return { refreshToken: ret };
+  }
+
+  @UseGuards(AuthGuard("jwt"))
+  @Get("auth/logout")
+  async logout(@Request() req, @Res({ passthrough: true }) res) {
+    res.clearCookie("refreshToken");
+    await this.authService.logout(req.user);
+    return { message: "success" };
+  }
+
+  @UseGuards(AuthGuard("jwt"))
+  @Get("auth/user")
+  getProfile(@Request() req) {
+    return req.user;
+  }
+
+  @UseGuards(AuthGuard("jwt-refresh"))
+  @Get("auth/refresh")
+  async refreshTokens(@Request() req) {
+    const accessToken = await this.authService.getAccessToken(req.user);
+    return { accessToken: accessToken };
   }
 }
