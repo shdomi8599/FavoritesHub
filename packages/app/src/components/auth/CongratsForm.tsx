26d91b3958b85410f0993ca16fbcd2f2d07747be
shdomi8599 <shdomi@naver.com>
2023-10-07 21:02:30 +0900

    app 회원가입 성공 시 폭죽 효과 추가

--- packages/app/src/components/auth/CongratsForm.tsx
+++ packages/app/src/components/auth/CongratsForm.tsx
@@ -1,8 +1,13 @@
 import { AuthProps } from "@/types";
+import { confettiRealisticLook } from "@/util";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import { useEffect } from "react";
 
 export default function CongratsForm({ handleAuthModal }: AuthProps) {
+  useEffect(() => {
+    confettiRealisticLook();
+  }, []);
   return (
     <Container component="main" maxWidth="xs">
       <Box
--- packages/app/src/util/index.ts
+++ packages/app/src/util/index.ts
@@ -1,3 +1,4 @@
 import { api } from "./api";
+import { confettiRealisticLook } from "./confetti";
 
-export { api };
+export { api, confettiRealisticLook };
