5294a986a5b86962394c193f3ab985d9ee2d3421
shdomi8599 <shdomi@naver.com>
2023-10-08 18:42:06 +0900

    api 노드메일러 환경변수 관리

--- packages/api/src/constants.ts
+++ packages/api/src/constants.ts
@@ -1,3 +1,7 @@
+import "dotenv/config";
+
+const { NODE_MAILER_USER, NODE_MAILER_PASS } = process.env;
+
 export const JWT_ACCESS_SECRET = "S+Hcvoy/Z08Ljqd7qglf8w1l+bbCjuvq30mQ3cZP21Q=";
 export const JWT_REFRESH_SECRET =
   "zeOPjBWjs8kVK3dsCi9OFc24CIN/VTZ3Svw6jXv7MF4=";
@@ -5,7 +9,7 @@ export const JWT_REFRESH_SECRET =
 export const nodemailerOption = {
   service: "gmail",
   auth: {
-    user: "shdomi8599@gmail.com",
-    pass: "biui osae gmxe escq",
+    user: NODE_MAILER_USER,
+    pass: NODE_MAILER_PASS,
   },
 };
