b12a7a10ffe55c4ca5fae05bb4bcc7c01d96ecb6
shdomi8599 <shdomi@naver.com>
2023-10-18 07:04:04 +0900

    app 메타 데이터 등록

--- packages/app/src/pages/_document.tsx
+++ packages/app/src/pages/_document.tsx
@@ -4,6 +4,25 @@ export default function Document() {
   return (
     <Html lang="ko">
       <Head>
+        <meta
+          name="description"
+          content="자신만의 즐겨찾기를 등록할 수 있는 FavoritesHub입니다."
+        />
+        <meta property="og:title" content="FavoritesHub" />
+        <meta
+          property="og:description"
+          content="자신만의 즐겨찾기를 등록할 수 있는 FavoritesHub입니다."
+        />
+        <meta property="og:image" content="/logo/favicon.ico" />
+        <meta property="og:url" content="https://favoriteshub.com/" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="FavoritesHub" />
+        <meta
+          name="twitter:description"
+          content="자신만의 즐겨찾기를 등록할 수 있는 FavoritesHub입니다."
+        />
+        <meta name="twitter:image" content="/logo/favicon.ico" />
+        <link rel="canonical" href="https://favoriteshub.com/" />
         <link rel="icon" href="/logo/favicon.ico" />
       </Head>
       <body>
