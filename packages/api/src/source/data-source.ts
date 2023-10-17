968d22a79d87a8602dec96b952a5657b5fe56daf
shdomi8599 <shdomi@naver.com>
2023-10-18 05:18:49 +0900

    api 디비 사용 주석 추가

--- packages/api/src/source/data-source.ts
+++ packages/api/src/source/data-source.ts
@@ -1,6 +1,12 @@
 import { DataSource, DataSourceOptions } from "typeorm";
 
-// docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=a7BSQzn5PaQVCfkX -d mysql:5.7
+//  AWS
+// docker run --platform linux/amd64 --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=a7BSQzn5PaQVCfkX -d mysql:5.7
+
+// M1칩 arm64v8 사용 < 오라클
+// sudo docker pull arm64v8/mysql
+// sudo docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=a7BSQzn5PaQVCfkX -d arm64v8/mysql
+
 export const dataSourceOptions: DataSourceOptions = {
   type: "mysql",
   host: "127.0.0.1",
