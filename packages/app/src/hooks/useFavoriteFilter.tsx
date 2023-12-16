ee41657acf3e31eec5dc8bbadcc99a1dcb9eb816
shdomi8599 <shdomi@naver.com>
2023-12-17 04:39:09 +0900

    디버깅 코드 삭제

--- packages/app/src/hooks/useFavoriteFilter.tsx
+++ packages/app/src/hooks/useFavoriteFilter.tsx
@@ -18,11 +18,9 @@ export const useFavoriteFilter = ({
 }: Props) => {
   const getFilterData = () => {
     let data: Favorite[] = [];
-    console.log(favorites);
 
     if (selectValue === "createdAt") {
       data = favorites?.sort((a, b) => {
-        console.log(new Date(b.createdAt).getTime());
         return (
           new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
         );
