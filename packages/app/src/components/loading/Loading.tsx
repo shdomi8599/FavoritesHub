a4491f84ee17beb6ee7d68fdd278c7aec07505e8
shdomi8599 <shdomi@naver.com>
2025-02-17 11:44:54 +0900

    로딩 컴포넌트 랜더링 방식 변경

--- packages/app/src/components/loading/Loading.tsx
+++ packages/app/src/components/loading/Loading.tsx
@@ -4,21 +4,23 @@ import { useRecoilValue } from "recoil";
 
 export default function Loading() {
   const isLoading = useRecoilValue(isLoadingState);
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "100vw",
-        height: "100vh",
-        position: "fixed",
-        backgroundColor: "black",
-        opacity: "0.5",
-        zIndex: isLoading ? 9999 : -100,
-      }}
-    >
-      <CircularProgress />
-    </Box>
-  );
+  if (isLoading)
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100vw",
+          height: "100vh",
+          position: "fixed",
+          backgroundColor: "black",
+          opacity: "0.5",
+          zIndex: 9999,
+        }}
+      >
+        <CircularProgress />
+      </Box>
+    );
+  return <></>;
 }
